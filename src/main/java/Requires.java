6415d96e3707f81bba53d57a342f89af12a9ba9d
Irmantas Kaunietis <kirmantas74@gmail.com>
2020-08-01 12:29:38 +0300

    Day 1 example of person

---
+++ src/main/java/Requires.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+
+public class Requires {
+    public static class Str {
+        public static void notNullOrEmpty(String value, String argumentName) {
+            if (value == null | value.isEmpty()) {
+                throw new IllegalArgumentException(argumentName + " cannot be null or empty");
+            }
+        }
+    }
+
+    public static class DateTime {
+        public static void NotFuture(LocalDate dateTime, String argumentName) {
+//            if (dateTime.compareTo(LocalDate.now()) > 0) {
+            boolean isFutureDateTime = dateTime.isAfter(LocalDate.now());
+            if (isFutureDateTime) {
+                throw new FutureBirthdayException(dateTime, argumentName);
+            }
+        }
+    }
+}
